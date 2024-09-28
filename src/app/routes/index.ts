bec261fe92db4bfe0e8d922c185877045d3f9c26
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-29 01:38:03 +0600

    payment success and error page created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,32 @@
-import express from "express";
+import express from 'express'
+import authRoute from "../modules/auth/auth.route";
+import userRoute from "../modules/user/user.route";
 
 const router = express.Router();
 
-// const moduleRoutes = [
-//   {
-//     path: "/users",
-//     route: UserRoutes,
-//   }
-// ];
+const moduleRoute = [
+  {
+    path: "/auth",
+    route: authRoute,
+  },
+  // {
+  //   path: "/payment",
+  //   route: paymentRoute,
+  // },
+  {
+    path: "/user",
+    route: userRoute,
+  },
+  // {
+  //   path: "/post",
+  //   route: postRoute,
+  // },
+  // {
+  //   path: "/category",
+  //   route: categoryRoute,
+  // },
+];
 
-// moduleRoutes.forEach((e) => router.use(e.path, e.route));
+moduleRoute.forEach((route) => router.use(route.path, route.route));
 
 export default router;
